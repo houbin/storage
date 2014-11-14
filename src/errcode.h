eabda0c42c14ba0b042cd016db24101ea9e1b02a
root <root@ross.(none)>
2014-11-14 16:16:10 +0800

    1. use leveldb log 2. create logger_test.cc

---
+++ src/errcode.h
@@ -0,0 +1,13 @@
+#ifndef STORAGE_ERRCODE_H_
+#define STORAGE_ERRCODE_H_
+
+#include <errno.h>
+
+#define OK                          0
+
+#define ERR_SELF_DEFINE_START       2000
+#define ERR_INVALID_INPUT_PARA      2001
+#define ERR_FILE_NOT_EXIST          2002
+#define ERR_OPEN_FILE               2003
+
+#endif
