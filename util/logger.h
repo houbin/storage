dbdf1da961c3adf531385bc6ffec1ed2d1d9d760
vincent178 <vincent.007.cn@gmail.com>
2014-12-01 09:03:28 +0800

    1. add util 2. add lib and gtest

---
+++ util/logger.h
@@ -0,0 +1,136 @@
+#ifndef STORAGE_LOGGER_H_
+#define STORAGE_LOGGER_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <sys/time.h>
+#include <time.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <stdarg.h>
+#include <assert.h>
+#include "errcode.h"
+
+namespace storage {
+
+class Logger
+{
+private:
+    FILE *file_;
+public:
+    Logger(FILE *f): file_(f) {}
+
+    virtual ~Logger()
+    {
+        fclose(file_);
+    }
+
+    void Logv(const char* format, va_list ap)
+    {
+        char buffer[500];
+        char *base;
+        int32_t size;
+
+        const uint64_t thread_id = pthread_self();
+        for (int iter = 0; iter < 2; iter++)
+        {
+            if (iter == 0)
+            {
+                size = sizeof(buffer);
+                base = buffer;
+            }
+            else
+            {
+                size = 30000;
+                base = new char[30000];
+            }
+
+            char *p = base;
+            char *limit = p + size;
+            
+            struct timeval now;
+            gettimeofday(&now, NULL);
+            time_t sec = now.tv_sec;
+            struct tm t;
+            localtime_r(&sec, &t);
+            
+            p += snprintf(p, limit - p, 
+                         "%04d/%02d/%02d-%02d:%02d:%02d.%06d %llx ",
+                         t.tm_year + 1970,
+                         t.tm_mon + 1,
+                         t.tm_mday,
+                         t.tm_hour,
+                         t.tm_min,
+                         t.tm_sec,
+                         static_cast<int>(now.tv_usec),
+                         static_cast<long long unsigned int>(thread_id)
+                        );
+            
+            if (p < limit)
+            {
+                va_list backup_ap;
+                va_copy(backup_ap, ap);
+                p += vsnprintf(p, limit - p, format, backup_ap);
+                va_end(backup_ap);
+            }
+
+            if (p >= limit)
+            {
+                if (iter == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    p = limit - 1;
+                }
+            }
+            
+            if (p == base || p[-1] != '\n')
+            {
+                *p++ = '\n';
+            }
+
+            assert(p <= limit);
+            fwrite(base, 1, p - base, file_);
+            fflush(file_);
+            
+            if (base != buffer)
+            {
+                delete[] base;
+            }
+            
+            break;
+        }
+    }
+};
+
+int32_t NewLogger(const char* name, Logger **logger)
+{
+    FILE *f = fopen(name, "w");
+    if (NULL == f)
+    {
+        return -ERR_OPEN_FILE;
+    }
+
+    *logger = new Logger(f);
+    return OK;
+}
+
+void Log_(Logger *info_log, const char* format, ...)
+{
+    if (NULL != info_log)
+    {
+        va_list ap;
+        va_start(ap, format);
+        info_log->Logv(format, ap);
+        va_end(ap);
+    }
+}
+
+#define Log(info_log, format, ...) Log_(info_log, "[Function: %s, Line: %d]: "format, __func__, __LINE__, ##__VA_ARGS__)
+
+}
+
+#endif
