af5d3890ce0790bf12481e424b59dfafae869003
houbin <houbin0504@163.com>
2014-12-03 11:13:00 +0800

    1. finish feature of disk initial 2. test basic function of disk initial ok

--- DiskFormat/umount_all_points.sh
+++ DiskFormat/umount_all_points.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#bin/sh
 # umount all mount points that belongs to our
 # umount /jovision/*
-MOUNT_PARENT_DIR=/jovision/
+MOUNT_PARENT_DIR=/jovision
 
 if [ -f ${MOUNT_PARENT_DIR} ];then
     echo "${MOUNT_PARENT_DIR} is a file"
     exit 1
-elseif [ ! -d ${MOUNT_PARENT_DIR} ]; then
+elif [ ! -d ${MOUNT_PARENT_DIR} ]; then
     mkdir /jovision/
 else
     umount ${MOUNT_PARENT_DIR}/*
