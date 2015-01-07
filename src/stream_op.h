4b8649029041af29a4c97d009466cf08a836669b
houbin <houbin0504@163.com>
2015-01-07 14:20:57 +0800

    1. add stream_op.h and stream_op.cc

---
+++ src/stream_op.h
@@ -0,0 +1,34 @@
+#ifndef STORAGE_STREAM_OP_H_
+#define STORAGE_STREAM_OP_H_
+
+#include "stream_info.h"
+
+namespace storage
+{
+
+enum StreamOpType{
+    STREAM_OP_ADD = 0,
+    STREAM_OP_DEL = 1
+};
+
+class StreamOp
+{
+private:
+    uint64_t op_seq_;
+    StreamInfo stream_info_;
+    StreamOpType stream_op_type_;
+    
+    // no copying allowed
+    StreamOp(const StreamOp&);
+    void operator=(const StreamOp&);
+public:
+    StreamOP(uint64_t op_seq_, StreamInfo &stream_info, StreamOpType stream_op_type)
+    : op_seq_(op_seq), stream_op_type_(stream_op_type)
+    {
+        stream_info_ = stream_info;
+    }
+};
+
+}
+
+#endif
